70a1438c9fd7bca8105456e537672e78b4da1fff
Anietie Brownson <anni.brownson@gmail.com>
2025-01-03 18:01:14 +0100

    feat: Add new code changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import Hero from "@/public/images/shadows.png";
-import HeroMobile from "@/public/images/shadows-mobile.png"
-import Abstract from "@/public/images/abstract.png"
-import AbstractStory from "@/public/images/abstract-story.png"
-import String from "@/public/images/vector-string.png"
-import DexscrLogo from "@/public/icons/dexscreener-black.png"
-import DextLogo from "@/public/icons/dextools.svg"
-import CopyAddressButton from "./components/CopyAddressButton"
+import HeroMobile from "@/public/images/shadows-mobile.png";
+import Abstract from "@/public/images/abstract.png";
+import AbstractStory from "@/public/images/abstract-story.png";
+import String from "@/public/images/vector-string.png";
+import DexscrLogo from "@/public/icons/dexscreener-black.png";
+import DextLogo from "@/public/icons/dextools.svg";
+import CopyAddressButton from "./components/CopyAddressButton";
 import OriginCards from "./components/OriginCards";
 import TokenomicsCards from "./components/TokenomicsCards";
-import TokenomicsHeroImg from "@/public/images/tokenomics-hero-img.png"
-import VectorUp from "@/public/icons/Vector-up.png"
+import TokenomicsHeroImg from "@/public/images/tokenomics-hero-img.png";
+import VectorUp from "@/public/icons/Vector-up.png";
 import MediaFeed from "./components/MediaFeed";
 import TextCarousel from "./components/TextCarousel";
 
@@ -31,7 +31,11 @@ export default function Home() {
         </div>
 
         <div className="flex items-center justify-center -mt-10 lg:-mt-44 ">
-          <Image src={Hero} alt="Hero" className="hidden lg:block lg:max-w-[1200px] lg:mx-auto" />
+          <Image
+            src={Hero}
+            alt="Hero"
+            className="hidden lg:block lg:max-w-[1200px] lg:mx-auto"
+          />
           <Image src={HeroMobile} alt="Hero" className="lg:hidden" />
         </div>
 
@@ -41,9 +45,51 @@ export default function Home() {
             sovereign ruler of the mythical Kekistani Empire, a realm that
             exists primarily in the digital universe of memes.
           </p>
+        </div>
 
-          <div className="flex justify-center items-center space-x-4 lg:space-x-8 mt-5 lg:mt-10">
-          <Link href="https://www.dextools.io/app/en/xrpl/pair-explorer/4B454B4955530000000000000000000000000000.rLWCx7obzMRbFfreNR6eScPz6GWj4xbr4v_XRP?t=1735732303613" rel="noopener noreferrer" target="_blank">
+        {/* floating images */}
+        <div className="hidden lg:block">
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute top-28 right-40"
+          />
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute top-60 left-32"
+          />
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute top-96 right-72"
+          />
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute bottom-96 left-60"
+          />
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute bottom-80 right-0"
+          />
+          <Image
+            src={Abstract}
+            alt=""
+            className="w-24 h-24 absolute bottom-24 right-40"
+          />
+        </div>
+      </section>
+
+        {/* buttons */}
+      <section className="px-6 lg:px-0">
+        <div className="flex justify-center items-center space-x-4 lg:space-x-8 mt-5 lg:mt-10">
+          <Link
+            href="https://www.dextools.io/app/en/xrpl/pair-explorer/4B454B4955530000000000000000000000000000.rLWCx7obzMRbFfreNR6eScPz6GWj4xbr4v_XRP?t=1735732303613"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <button className="bg-[#FFEB3B] max-w-[200px] text-[#2E7D32] text-sm lg:text-base shadow-dextoolShadow border-[1px] border-b-2 border-[#2E7D32] font-fredoka font-semibold px-8 h-14 py-3 flex items-center rounded-lg">
               DEXTOOLS
               <Image
@@ -52,9 +98,13 @@ export default function Home() {
                 className="w-5 h-5 ml-2"
               />
             </button>
-            </Link>
+          </Link>
 
-            <Link href="https://t.co/znB3HYcyF8" rel="noopener noreferrer" target="_blank">
+          <Link
+            href="https://t.co/znB3HYcyF8"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <button className="bg-[#FFFFFF] w-[180px] flex justify-center text-[#000000] text-sm lg:text-base shadow-dexscreenerShadow border-[1px] border-b-2 border-[#000000] font-fredoka font-semibold px-8 h-14 py-3 items-center rounded-lg">
               DEXSCREENER
               <Image
@@ -63,18 +113,7 @@ export default function Home() {
                 className="w-6 h-6 ml-2 rounded-full"
               />
             </button>
-            </Link>
-          </div>
-        </div>
-
-        {/* floating images */}
-        <div className="hidden lg:block">
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute top-28 right-40"/>
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute top-60 left-32"/>
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute top-96 right-72"/>
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute bottom-96 left-60"/>
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute bottom-80 right-0"/>
-          <Image src={Abstract} alt="" className="w-24 h-24 absolute bottom-24 right-40"/>
+          </Link>
         </div>
       </section>
 
@@ -83,15 +122,18 @@ export default function Home() {
         <TextCarousel />
       </section>
 
-      {/* floating string */}
-      <section>
-
-      </section>
-
       {/* story section */}
       <section className="px-6 lg:px-0 mt-20 flex flex-col justify-center items-center">
-        <Image src={AbstractStory} alt="" className="w-96 h-60 lg:h-80 object-contain -mt-14 -mb-28 -z-30" />
-        <Image src={String} alt="" className="absolute -translate-x-[26rem] -translate-y-80 w-96 h-[20.5rem] object-contain -mt-14" />
+        <Image
+          src={AbstractStory}
+          alt=""
+          className="w-96 h-60 lg:h-80 object-contain -mt-14 -mb-28 -z-30"
+        />
+        <Image
+          src={String}
+          alt=""
+          className="absolute -translate-x-[26rem] -translate-y-80 w-96 h-[20.5rem] object-contain -mt-14"
+        />
 
         <div className="container flex flex-col items-center justify-center py-7 lg:py-14 px-4 lg:px-10 rounded-lg lg:rounded-[2rem] bg-[#E0CDA9] max-w-[800px]">
           <h1 className="font-luckiestGuy text-[#3D3D3D] text-2xl lg:text-4xl text-center mb-5">
@@ -128,12 +170,14 @@ export default function Home() {
         </div>
       </section>
 
-        {/* floating image absolute -translate-x-[32rem] -translate-y-64 lg:w-[500px] lg:h-80*/}
-        <div>
-        <Image src={AbstractStory} alt="" className="-mt-40 lg:-mt-64 lg:ml-32 hidden lg:block lg:w-[500px] lg:h-80" />
-        </div>
-
-        
+      {/* floating image absolute -translate-x-[32rem] -translate-y-64 lg:w-[500px] lg:h-80*/}
+      <div>
+        <Image
+          src={AbstractStory}
+          alt=""
+          className="-mt-40 lg:-mt-64 lg:ml-32 hidden lg:block lg:w-[500px] lg:h-80"
+        />
+      </div>
 
       {/* origin section */}
       <section className="mt-20 bg-[#8FE896] px-6 lg:px-24 py-7 lg:py-12">
@@ -147,7 +191,7 @@ export default function Home() {
       <section className="px-6 lg:pr-0 lg:pl-24">
         <div className="lg:flex lg:flex-row lg:justify-between lg:items-center">
           <div>
-          <h1 className="font-luckiestGuy text-[#3D3D3D] text-3xl lg:text-6xl mt-5 mb-3 lg:mt-10 text-center lg:text-left">
+            <h1 className="font-luckiestGuy text-[#3D3D3D] text-3xl lg:text-6xl mt-5 mb-3 lg:mt-10 text-center lg:text-left">
               TOKENOMICS
             </h1>
             <p className="font-fredoka text-[#3D3D3D] font-medium text-xl max-w-[828px] mb-5 text-center lg:text-left">
@@ -155,12 +199,16 @@ export default function Home() {
             </p>
 
             <span>
-            <TokenomicsCards />
+              <TokenomicsCards />
             </span>
           </div>
 
           <div>
-          <Image src={TokenomicsHeroImg} alt="" className="hidden lg:block max-w-[500px]" />
+            <Image
+              src={TokenomicsHeroImg}
+              alt=""
+              className="hidden lg:block max-w-[500px]"
+            />
           </div>
         </div>
       </section>
